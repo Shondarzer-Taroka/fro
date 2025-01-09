26762e9aadb438db992cf784ac9843c80ab3c8be
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-01-09 12:21:17 +0600

    users are put in Users component

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -1,15 +1,25 @@
+import { User } from '@/models/Users';
 import React from 'react';
 
 async function getUser() {
-    const res=await fetch('https://jsonplaceholder.typicode.com/users',{cache:'no-cache'})
-    const data=await res.json()
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-cache' })
+    const data = await res.json()
     return data
 }
 
-const Users = () => {
+const Users = async () => {
+    const users: User[] = await getUser()
+    console.log(users);
+
+
     return (
         <div>
-            
+            {users.length === 0 && <h1>data is coming, please wait.......!</h1>}
+            {
+                users.map((user) => <div key={user.id}>
+                    <p>{user.name}</p>
+                </div>)
+            }
         </div>
     );
 };
